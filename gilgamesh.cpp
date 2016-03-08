0e587c83fb7b25f334c8aa6de9862d0d4536a140
Andrea Orru <andreaorru1991@gmail.com>
2016-03-08 04:18:01 +0000

    Small cosmetic change.

--- gilgamesh.cpp
+++ gilgamesh.cpp
@@ -284,7 +284,7 @@ void GilgameshTrace(uint8 Bank, uint16 Address)
 	 * - Otherwise, create an "empty" instruction with the given PC.
 	 * Then decode the instruction. */
 	Instructions.emplace(std::piecewise_construct,
-	             		 std::forward_as_tuple(PC),
+						 std::forward_as_tuple(PC),
 						 std::forward_as_tuple(PC)).first->second.Decode();
 }
 
